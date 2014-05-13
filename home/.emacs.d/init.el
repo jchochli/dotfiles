a8cb7bd23308b18df47bce7609a1a87ff7eaeee3
James Chochlinski <jchochlinski@xpzen.com>
2014-05-13 07:00:06 -0400

    added clojure profiles

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -3,7 +3,7 @@
 
 ;; Add the Melpa repository to the list of package sources
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			 ("marmalade" . "http://marmalade-repo.org/packages/")
+			 ;;("marmalade" . "http://marmalade-repo.org/packages/")
 			 ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 ;; Initialise the package system.
