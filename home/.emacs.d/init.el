a4de12b8ae3935a296f9fe4ab99e5cfdf7e7142b
James Chochlinski <jchochlinski@xpzen.com>
2014-06-18 09:19:05 -0400

    enable 4clojure

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -17,11 +17,42 @@
 		      clojure-mode
 		      clojure-test-mode
 		      cider
+          magit
           ahg
           emacs-eclim
           4clojure))
 
 ;; multiple-cursors and expand-region?
+(defadvice 4clojure-open-question (around 4clojure-open-question-around)
+  "Start a cider/nREPL connection if one hasn't already been started when
+opening 4clojure questions"
+  ad-do-it
+  (unless cider-current-clojure-buffer
+    (cider-jack-in)))
+
+(defun 4clojure-login (user pwd)
+  "Login to 4clojure"
+  (interactive "sWhat's your name? \nsAnd your password ")
+  (request
+   "http://www.4clojure.com/login"
+   :type "POST"
+   :sync t
+   :headers '(
+              ("User-Agent" . "Mozilla/5.0 (X11; Linux x86_64; rv:28.0) Gecko/20100101  Firefox/28.0")
+              ("Referer" . "http://www.4clojure.com/login")
+              )
+;   :parser 'buffer-string
+   :data `(("user" . ,user) ("pwd" . ,pwd))
+   :success (function*
+             (lambda (&key data &allow-other-keys)
+               data
+               )
+             )
+; when server send 302 header, `request` redirect request with original method POST, 
+; So 4clojure will not handle this redirect and given 404
+   :status-code '((404 . (lambda (&rest _) (message "login successful!"))))
+   )
+  )
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
