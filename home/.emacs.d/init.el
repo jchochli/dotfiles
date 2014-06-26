d65e74a9c74f2619ae9da2bbe6abf300638fb78c
James Chochlinski <jchochlinski@xpzen.com>
2014-06-26 10:26:07 -0400

    emacs blinking cursor, hg merge with emacs, turn off osx secure emptytrash

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -87,6 +87,7 @@ opening 4clojure questions"
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode t)
  '(custom-enabled-themes (quote (graphene wombat)))
  '(custom-safe-themes
    (quote
