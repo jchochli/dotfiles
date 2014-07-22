fcd3d9d06e2e163dabe8714c87c62a95d73fda64
James Chochlinski <jchochlinski@xpzen.com>
2014-07-22 07:11:11 -0400

    move faster

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -126,6 +126,27 @@
 (global-set-key (kbd "<C-S-left>")   'buf-move-left)
 (global-set-key (kbd "<C-S-right>")  'buf-move-right)
 
+;; Move more quickly
+(global-set-key (kbd "C-S-n")
+                (lambda ()
+                  (interactive)
+                  (ignore-errors (next-line 5))))
+
+(global-set-key (kbd "C-S-p")
+                (lambda ()
+                  (interactive)
+                  (ignore-errors (previous-line 5))))
+
+(global-set-key (kbd "C-S-f")
+                (lambda ()
+                  (interactive)
+                  (ignore-errors (forward-char 5))))
+
+(global-set-key (kbd "C-S-b")
+                (lambda ()
+                  (interactive)
+                  (ignore-errors (backward-char 5))))
+
 ;;
 ;; ace jump mode major function
 ;; 
