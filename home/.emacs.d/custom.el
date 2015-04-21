6bc7c783e035e82b76a177e53fae26e4aee882bc
James Chochlinski <jchochlinski@xpzen.com>
2015-04-20 22:10:14 -0400

    tmux config and colors

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(blink-cursor-mode t)
  '(bmkp-last-as-first-bookmark-file "/Users/jameschochlinski/.emacs.d/bookmarks")
  '(browse-kill-ring-highlight-current-entry t)
@@ -10,7 +12,7 @@
  '(custom-enabled-themes (quote (wombat)))
  '(custom-safe-themes
    (quote
-    ("ebd976c3d91de7c6858db3ba07c52238d545a106c8fbf05192913f6d42421621" "f1ea873350bbb910a551854d700dfa7a16f0b6e7b9e88e12e012d9f0f881d083" default)))
+    ("9122dfb203945f6e84b0de66d11a97de6c9edf28b3b5db772472e4beccc6b3c5" "ebd976c3d91de7c6858db3ba07c52238d545a106c8fbf05192913f6d42421621" "f1ea873350bbb910a551854d700dfa7a16f0b6e7b9e88e12e012d9f0f881d083" default)))
  '(desktop-save-mode t)
  '(eclim-eclipse-dirs (quote ("~/Development/bin/eclipse")))
  '(eclim-executable "~/Development/bin/eclipse/eclim")
