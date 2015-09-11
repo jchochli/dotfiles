95ed7e7c080f29cd8be487cb44a3984c28875b70
James Chochlinski <jchochli@xpzen.com>
2015-09-11 10:18:34 -0400

    add lein-immutant

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user {:plugins [[cider/cider-nrepl "0.9.1"]
-                  [refactor-nrepl "1.1.0"]]
+                  [refactor-nrepl "1.1.0"]
+                  [lein-immutant "2.0.0"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}
 
 
