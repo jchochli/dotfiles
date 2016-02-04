8e31b37716a90db1ce57dd0163d58929dff1f475
James Chochlinski <jchochli@xpzen.com>
2016-02-04 09:59:09 -0500

    cider-nrepl 0.11.0-snapshot

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [[refactor-nrepl "2.0.0-SNAPSHOT"]
-                  [cider/cider-nrepl "0.10.2"]]                  
+                  [cider/cider-nrepl "0.11.0-SNAPSHOT"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
 
 
